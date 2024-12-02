c0e07d0231833ad616f718ea0fda79d1779572e7
nori-ok <ayumanodev@gmail.com>
2024-12-02 20:55:24 +0900

    update:初めての更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def hola
+        render html: "hola,mundo!"
+    end
+
+    def goodbye
+        render html: "goodbye,world!"
+    end
 end
